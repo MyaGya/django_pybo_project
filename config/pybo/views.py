71a6f64e2ac50fca5166a1a1ef5315e561eae3dc
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-10-20 23:55:34 +0900

    질문 상세 내용 구현 및 views 함수 수정, urls name 부여 및 url 하드코딩 수정

--- config/pybo/views.py
+++ config/pybo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
-
+from django.shortcuts import get_object_or_404
 
 def index(request):
     '''
@@ -15,6 +15,7 @@ def detail(request, question_id):
     """
     pybo 내용 출력
     """
-    question = Question.objects.get(id=question_id)
+    #question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
